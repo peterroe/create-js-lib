df3c43dd24373b94a16414a0e91cc45c28fe0dd5
Peter roe <peterroe@163.com>
2021-12-16 22:04:00 +0800

    update

---
+++ bin/init.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+console.log('Hello:', process.argv[2])
---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { terser } from 'rollup-plugin-terser'
+export default defineConfig({
+    plugins: [vue()],
+    build: {
+        lib: {
+            entry: './lib/index.js',
+            formats: ['es', 'umd'],
+            name: 'cjl'
+        },
+        rollupOptions: {
+            plugins: [
+                tenser()
+            ]
+        }
+    }
+})
